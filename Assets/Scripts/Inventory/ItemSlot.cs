e6e0c94f78aaee9cff1202a1c81dec7cd65b13fb
Peter Sutarjo <peter.sutarjo@gmail.com>
2021-10-03 09:05:51 -0700

    added item slot

---
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemSlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop");
+        if (eventData.pointerDrag != null) {
+            Debug.Log(eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition);
+            Debug.Log(GetComponent<RectTransform>().anchoredPosition);
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+        }
+    }
+}
