c0bfc1f3e79ea3d6eb2bde522d86dec4fa1abcdb
Richard Cheng <richard.hokan.cheng@gmail.com>
2021-10-03 01:34:57 -0700

    Proof of concept dialogue interactable

--- Assets/Scripts/Top-Down/Proof of Concept Interactables/Interactable.cs
+++ Assets/Scripts/Top-Down/Proof of Concept Interactables/Interactable.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public abstract class Interactable : MonoBehaviour
 {
-	public string nodeName;	// Name of the Yarn node to start when this is interacted with
     public abstract void interact();
 }
