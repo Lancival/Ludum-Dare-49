84a57067b30a9c4ecbeb0ad541daf313bd9bb78b
Austin Law <austinlaw8@gmail.com>
2021-10-03 00:19:53 -0700

    Fixed merge and created Harper prefab

--- Assets/Scripts/Top-Down/CameraMove.cs
+++ Assets/Scripts/Top-Down/CameraMove.cs
@@ -7,8 +7,6 @@ public class CameraMove : MonoBehaviour
 {
     [SerializeField] public Camera mainCamera;
     [SerializeField] private float duration;
-
-<<<<<<< HEAD
     [SerializeField] private float zoomFactor;
 
     public IEnumerator ZoomIn(){
@@ -24,16 +22,11 @@ public class CameraMove : MonoBehaviour
           yield return null;
         }
         Debug.Log("Zoom Complete");
-=======
-    public void ZoomIn() {
-        if(mainCamera.orthographicSize > 1)
-            mainCamera.orthographicSize -= 0.01f;
->>>>>>> ca192580498a06c130e14ea6d94e3375582f90a4
     }
 
     void Awake(){
       Scene scene = SceneManager.GetActiveScene();
-      
+
       if (scene.name == "House")
         this.enabled = false;
     }
@@ -42,26 +35,15 @@ public class CameraMove : MonoBehaviour
     void Start()
     {
         if (mainCamera == null)
-<<<<<<< HEAD
-		{
-			Debug.Log("Error: Main camera not provided to CameraMove scipt.");
-			Destroy(this);  
-		}
-=======
-		      {
-			         Debug.Log("Error: Main camera not provided to CameraMove script.");
-			         Destroy(this.gameObject);
-		      }
->>>>>>> ca192580498a06c130e14ea6d94e3375582f90a4
+		    {
+			       Debug.Log("Error: Main camera not provided to CameraMove scipt.");
+			       Destroy(this);
+		    }
     }
 
     // Update is called once per frame
     void Update()
     {
        	mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, mainCamera.transform.position.z);
-<<<<<<< HEAD
-        //ZoomIn();
-=======
->>>>>>> bddee269f84a1d08faecef0dec76110d33684077
     }
 }
