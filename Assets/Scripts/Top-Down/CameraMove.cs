74de8d0139655cbacf214ac829192f4dc9eaa911
Conway Xu <conwayxu93@gmail.com>
2021-10-03 03:10:51 -0400

    zoom in before scene transition

--- Assets/Scripts/Top-Down/CameraMove.cs
+++ Assets/Scripts/Top-Down/CameraMove.cs
@@ -5,17 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class CameraMove : MonoBehaviour
 {
-    [SerializeField] private Camera mainCamera;
+    [SerializeField] public Camera mainCamera;
+    [SerializeField] private float duration;
 
-    public void ZoomIn() {
-        if(mainCamera.orthographicSize > 1)
+    [SerializeField] private float zoomFactor;
+
+    public IEnumerator ZoomIn(){
+        float time = 0f;
+        float temp;
+        while (time < duration)
         {
-            mainCamera.orthographicSize -= 0.01f;
-        } 
+          temp = Time.deltaTime;
+          time += temp;
+          if (mainCamera.orthographicSize > 1) {
+              mainCamera.orthographicSize -= zoomFactor * Time.deltaTime;
+          }
+          yield return null;
+        }
+        Debug.Log("Zoom Complete");
     }
 
     void Awake(){
       Scene scene = SceneManager.GetActiveScene();
+      
       if (scene.name == "House")
       {
         gameObject.SetActive(false);
@@ -28,8 +40,8 @@ public class CameraMove : MonoBehaviour
     {
         if (mainCamera == null)
 		{
-			Debug.Log("Error: Main camera not provided to Player scipt.");
-			Destroy(this);
+			Debug.Log("Error: Main camera not provided to CameraMove scipt.");
+			Destroy(this);  
 		}
     }
 
@@ -37,6 +49,5 @@ public class CameraMove : MonoBehaviour
     void Update()
     {
        	mainCamera.transform.position = new Vector3(transform.position.x, transform.position.y, mainCamera.transform.position.z);
-        ZoomIn();
     }
 }
