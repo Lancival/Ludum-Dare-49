628e2060ca39b70f37ed09656f93d9c26600d224
Austin Law <austinlaw8@gmail.com>
2021-10-04 00:31:06 -0700

    Reorganized folders

---
+++ Assets/Scripts/Top-Down/HouseTransitioner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseTransitioner : MonoBehaviour
+{
+    [SerializeField] private int livingRoom;
+    [SerializeField] private int bedroom;
+    [SerializeField] private float duration;
+
+    private Collider2D livingRoomDoor;
+    private Collider2D bedroomDoor;
+    private bool inLivingRoom;
+    private float diff;
+    void Start(){
+      inLivingRoom = true;
+      diff = (bedroom - livingRoom) / duration;
+      Collider2D[] doors = this.GetComponents<BoxCollider2D>();
+      if (doors[0].offset.x < 0){
+        livingRoomDoor = doors[1];
+        bedroomDoor = doors[0];
+      }else{
+        livingRoomDoor = doors[0];
+        bedroomDoor = doors[1];
+      }
+      bedroomDoor.enabled = false;
+    }
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player"){
+          if (inLivingRoom){
+              StartCoroutine(other.GetComponent<CameraMove>().CamPan(diff, 0, 2f));
+              livingRoomDoor.enabled = false;
+              bedroomDoor.enabled = true;
+          }else{
+              StartCoroutine(other.GetComponent<CameraMove>().CamPan(-diff, 0, 2f));
+              livingRoomDoor.enabled = true;
+              bedroomDoor.enabled = false;
+          }
+          inLivingRoom = !inLivingRoom;
+        }
+    }
+}
