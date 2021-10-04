dcbfba36490b19ac629927640ee95428884c8750
Richard Cheng <richard.hokan.cheng@gmail.com>
2021-10-04 01:51:25 -0700

    Fixed dialogue control

--- Assets/Scripts/UI/DialogueControl.cs
+++ Assets/Scripts/UI/DialogueControl.cs
@@ -9,32 +9,49 @@ public class DialogueControl : MonoBehaviour
     [SerializeField] private GameObject player;
 
     private DialogueRunner runner;
+    private CustomUI ui;
     private Vector3 initPos;
-    private Vector3 playerPos;
+    private Vector3 offset = new Vector3(0, 1f, 0);
+
     // Start is called before the first frame update
     void Start()
     {
       runner = GameObject.FindObjectOfType<DialogueRunner>();
       if (runner == null)
       {
-        Debug.Log("DialogueInteractable unable to locate DialogueRunner.");
+        Debug.Log("DialogueControl unable to locate DialogueRunner.");
         Destroy(this);
       }
+      ui = GameObject.FindObjectOfType<CustomUI>();
+      if (ui == null)
+      {
+        Debug.Log("DialogueControl unable to locate CustomUI");
+        Destroy(this);
+      }
+
     	runner.AddCommandHandler("setSpeaker", SetSpeaker);
+
+      ui.onDialogueStart.AddListener(DisablePlayerControls);
+      ui.onDialogueStart.AddListener(SetInitPos);
+      ui.onDialogueEnd.AddListener(EnablePlayerControls);
     }
 
-    public void SetSpeaker(string[] parameters) {
-      runner.transform.position = parameters[0] == "MC" ? playerPos : initPos;
+    public void SetSpeaker(string[] parameters)
+    {
+      if (parameters != null)
+        runner.transform.position = (parameters[0] == "MC" ? player.transform.position + offset : initPos);
     }
 
-    public void SetInitPos(){
+    private void SetInitPos()
+    {
       initPos = runner.transform.position;
-      playerPos = player.transform.position + new Vector3(0,.33f,0);
     }
+
     public void EnablePlayerControls()
     {
       player.GetComponent<Player>().SetPlayerControls(true);
     }
+
     public void DisablePlayerControls()
     {
       player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
