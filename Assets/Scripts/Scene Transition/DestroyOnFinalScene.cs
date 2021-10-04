2857878d9e0177a750e7475c4adb9afd94d2b5a1
Peter Sutarjo <peter.sutarjo@gmail.com>
2021-10-04 16:31:49 -0700

    Update DestroyOnFinalScene.cs

--- Assets/Scripts/Scene Transition/DestroyOnFinalScene.cs
+++ Assets/Scripts/Scene Transition/DestroyOnFinalScene.cs
@@ -15,8 +15,15 @@ public class DestroyOnFinalScene : MonoBehaviour
         Debug.Log(SceneManager.GetActiveScene().name.ToLower());
         if (SceneManager.GetActiveScene().name.ToLower() == "finalscene")
         {
+            Debug.Log("attempting to destroy sfx");
+            Destroy(GameObject.Find("Sfx_ItemPickup"));
+            Destroy(GameObject.Find("QuestCompleteAudio"));
+            Destroy(GameObject.Find("AudioLayerManager"));
+            Debug.Log("attempting to destroy UI");
+            Destroy(GameObject.Find("UI"));
         	Debug.Log("attempting to destroy player");
         	Destroy(this.gameObject);
         }
+        
     }
 }
